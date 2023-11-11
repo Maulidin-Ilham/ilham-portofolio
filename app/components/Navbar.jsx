069e2bc7b5dba5254ea00c0dd3ab3123c2b0ba2d
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2023-11-11 19:48:14 +0700

    active link done

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,45 +1,55 @@
 "use client";
 import Link from "next/link";
 import React from "react";
-import { usePathname } from "next/navigation"; // Use useNavigation from next/navigation
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
-  const pathname = usePathname(); // Use useNavigation instead of useRouter
+  const pathname = usePathname();
 
   return (
     <div className="flex flex-col p-3 mt-12 mx-2 md:max-w-2xl md:mx-auto lg:max-w-3xl lg:mx-auto">
       <div className="flex flex-row space-x-5">
-        <Link
-          href={"/"}
-          className={`opacity-75 hover:opacity-100 transition duration-200 ${
-            pathname === "/" ? "opacity-100" : ""
-          }`}
-        >
-          <h1>home</h1>
+        <Link href={"/"}>
+          <h1
+            className={`  transition duration-200 ${
+              pathname === "/" ? "opacity-100" : " opacity-60 hover:opacity-100"
+            }`}
+          >
+            home
+          </h1>
         </Link>
-        <Link
-          href={"/about"}
-          className={`opacity-75 hover:opacity-100 transition duration-200 ${
-            pathname === "/about" ? "opacity-100" : ""
-          }`}
-        >
-          <h1>about</h1>
+        <Link href={"/about"}>
+          <h1
+            className={`  transition duration-200 ${
+              pathname === "/about"
+                ? "opacity-100"
+                : " opacity-60 hover:opacity-100"
+            }`}
+          >
+            about
+          </h1>
         </Link>
-        <Link
-          href={"/experience"}
-          className={`opacity-75 hover:opacity-100 transition duration-200 ${
-            pathname === "/experience" ? "opacity-100" : ""
-          }`}
-        >
-          <h1>experience</h1>
+        <Link href={"/experience"}>
+          <h1
+            className={`  transition duration-200 ${
+              pathname === "/experience"
+                ? "opacity-100"
+                : " opacity-60 hover:opacity-100"
+            }`}
+          >
+            experience
+          </h1>
         </Link>
-        <Link
-          href={"/project"}
-          className={`opacity-75 hover:opacity-100 transition duration-200 ${
-            pathname === "/project" ? "opacity-100" : ""
-          }`}
-        >
-          <h1>project</h1>
+        <Link href={"/project"}>
+          <h1
+            className={`  transition duration-200 ${
+              pathname === "/project"
+                ? "opacity-100"
+                : " opacity-60 hover:opacity-100"
+            }`}
+          >
+            project
+          </h1>
         </Link>
       </div>
     </div>
