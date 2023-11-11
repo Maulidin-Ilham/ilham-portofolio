601a0304bb7f17be3f49e947ec1e83ce5ea2a181
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2023-11-11 12:01:15 +0700

    adding active link in navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,31 +1,43 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation"; // Use useNavigation from next/navigation
 
 const Navbar = () => {
+  const pathname = usePathname(); // Use useNavigation instead of useRouter
+
   return (
     <div className="flex flex-col p-3 mt-12 mx-2 md:max-w-2xl md:mx-auto lg:max-w-3xl lg:mx-auto">
       <div className="flex flex-row space-x-5">
         <Link
           href={"/"}
-          className="opacity-75 hover:opacity-100 transition duration-200"
+          className={`opacity-75 hover:opacity-100 transition duration-200 ${
+            pathname === "/" ? "opacity-100" : ""
+          }`}
         >
           <h1>home</h1>
         </Link>
         <Link
           href={"/about"}
-          className="opacity-75 hover:opacity-100 transition duration-200"
+          className={`opacity-75 hover:opacity-100 transition duration-200 ${
+            pathname === "/about" ? "opacity-100" : ""
+          }`}
         >
           <h1>about</h1>
         </Link>
         <Link
           href={"/experience"}
-          className="opacity-75 hover:opacity-100 transition duration-200"
+          className={`opacity-75 hover:opacity-100 transition duration-200 ${
+            pathname === "/experience" ? "opacity-100" : ""
+          }`}
         >
           <h1>experience</h1>
         </Link>
         <Link
           href={"/project"}
-          className="opacity-75 hover:opacity-100 duration-200 "
+          className={`opacity-75 hover:opacity-100 transition duration-200 ${
+            pathname === "/project" ? "opacity-100" : ""
+          }`}
         >
           <h1>project</h1>
         </Link>
