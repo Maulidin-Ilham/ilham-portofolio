01501ed4b633f92c56c5e386cbd267c9ca480e3e
Maulidin-Ilham <ilhammaulidinilham7373@gmail.com>
2023-11-12 10:59:29 +0700

    project page done

--- app/project/page.jsx
+++ app/project/page.jsx
@@ -1,9 +1,85 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
   return (
-    <div className="flex flex-col p-3 mt-12 mx-2 md:max-w-2xl md:mx-auto lg:max-w-3xl lg:mx-auto">
-      <h1>This is project page</h1>
+    <div className="flex flex-col p-3 mt-12 mx-2 md:max-w-2xl md:mx-auto lg:max-w-3xl lg:mx-auto mb-3">
+      <h1 className="text-xl font-semibold">Projects</h1>
+      <h1 className="mt-4">
+        These are projects that i have created, hopefully these projects can
+        provide inspiration for all of you
+      </h1>
+      <div className="grid grid-cols-2 mt-8 gap-x-5 gap-y-8 w-full ">
+        <Link
+          className="flex flex-col  w-full "
+          href={"https://ilham-idcamp-note-app.vercel.app/"}
+          target="_blank"
+        >
+          <img
+            src={"/note-app.png"}
+            alt="note-app"
+            className="  object-cover rounded"
+          />
+          <h1 className=" font-semibold mt-2 lg:text-lg">Note Taking app</h1>
+          <p className="line-clamp-3">
+            The Dicoding ID Camp Final Project is an innovative note-taking
+            application developed using React.js and styled with Tailwind CSS.
+          </p>
+        </Link>
+        <Link
+          className="flex flex-col  w-full "
+          href={"https://fake-store-ilham.vercel.app/"}
+          target="_blank"
+        >
+          <img
+            src={"/fake-store.png"}
+            alt="fake-store"
+            className="  object-cover rounded"
+          />
+          <h1 className=" font-semibold mt-2 lg:text-lg">Fake Store</h1>
+          <p className="line-clamp-3">
+            Introducing a Next.js-powered fake store, where I leverage data from
+            a fake store API to create a dynamic and responsive web application.{" "}
+          </p>
+        </Link>
+        <Link
+          className="flex flex-col  w-full "
+          href={"https://anime-hub-ilham.vercel.app/"}
+          target="_blank"
+        >
+          <img
+            src={"/anime-hub.png"}
+            alt="anime-hub"
+            className="  object-cover rounded"
+          />
+          <h1 className=" font-semibold mt-2 lg:text-lg">Anime Hub</h1>
+          <p className="line-clamp-3">
+            Explore my anime list-inspired web application! Built with Next.js
+            and Talwind CSS, it dynamically fetches anime data from the Jikan
+            API, offering a user-friendly interface for anime enthusiasts to
+            discover and track their favorite shows.
+          </p>
+        </Link>
+        <Link
+          className="flex flex-col  w-full "
+          href={"https://quote-generator-maulidin-ilham.vercel.app/"}
+          target="_blank"
+        >
+          <img
+            src={"/quote-generator.png"}
+            alt="quote-generator"
+            className="  object-cover rounded"
+          />
+          <h1 className=" font-semibold mt-2 lg:text-lg">Quote generator</h1>
+          <p className="line-clamp-3">
+            Unleash inspiration with the Quote Generator! Powered by React.js,
+            Tailwind CSS and fueled by the TypeFit API, this web application
+            delivers a daily dose of motivation through dynamically fetched
+            quotes.{" "}
+          </p>
+        </Link>
+      </div>
     </div>
   );
 };
